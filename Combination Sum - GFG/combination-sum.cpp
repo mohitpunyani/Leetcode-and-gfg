72ad9325cc0ba0637ed17ccdc97484aca214b00c
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-04-23 14:47:32 +0530

    Added solution - LeetHub

--- Combination Sum - GFG/combination-sum.cpp
+++ Combination Sum - GFG/combination-sum.cpp
@@ -10,30 +10,52 @@ using namespace std;
 
 class Solution {
   public:
-
-vector<vector<int>> ans;
-vector<int>v;
-    void help(vector<int>& a ,int sum , int s , int e){
-        if(sum < 0){
-            return;
+    //Function to return a list of indexes denoting the required 
+    
+    void solve(vector<int>&A,int s,int sum,vector<int>&temp,vector<vector<int>>&ans){
+        
+        if(sum==0){
+            ans.push_back(temp);
+            return ;
         }
-        if(sum == 0){
-            ans.push_back(v);
-            return;
+        
+        if(sum<0){
+            return ;
         }
-        for(int i = s ; i<e; i++){
-            v.push_back(a[i]);
-            help(a , sum-a[i]  , i , e);
-            v.pop_back();
+        
+        for(int i=s;i<=A.size()-1;i++){
+           temp.push_back(A[i]);
+           solve(A,i,sum-A[i],temp,ans);
+           temp.pop_back();
         }
-        // return ;
+        return;
+        
     }
-    vector<vector<int> > combinationSum(vector<int> &a, int x) {
-        // Your code here
-        sort(a.begin() ,a.end());
-        a.resize(unique(a.begin() , a.end())-a.begin());
-        int n = a.size();
-        help(a , x ,0 , n );
+    vector<vector<int> > combinationSum(vector<int> &A, int B) 
+    {
+        // Your code here 
+        
+        // // we are hret o do so that there is not a god deal iwht out 
+        
+        sort(A.begin(),A.end());
+        vector<int>temp;
+        vector<vector<int>>ans;
+          A.resize(unique(A.begin() , A.end())-A.begin());
+        // A.erase(unique(A.begin(), A.end()), A.end()); // remove duplicate elements
+        sort(ans.begin(),ans.end());
+        solve(A,0,B,temp,ans);
+        // for(int i=0;i<ans.size();i++){
+        //     sort(ans[i].begin(),ans[i].end());
+        // }
+        // set<vector<int>>st;
+        // for(auto x:ans){
+        //     st.insert(x);
+        // }
+        // ans.clear();
+        // for(auto y:st){
+        //     ans.push_back(y);
+        // }
+        // sort(ans.begin(),ans.end());
         return ans;
     }
 };
