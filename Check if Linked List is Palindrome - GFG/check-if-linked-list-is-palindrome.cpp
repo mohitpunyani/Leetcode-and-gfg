44ffe7178029157ae99ce00f93cae4e924bc6ca0
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-04-25 15:18:17 +0530

    Added solution - LeetHub

--- Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -34,37 +34,88 @@ class Solution{
   public:
     //Function to check whether the list is palindrome.
     
+    Node* middleofll(Node*head){
+        Node*slow=head;
+        Node*fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            fast=fast->next;
+            slow=slow->next;
+            if(fast->next!=NULL){
+                fast=fast->next;
+            }
+        }
+        return slow;
+    }
+    Node*rev(Node*head){
+        Node*prev=NULL;
+        Node*curr=head;
+        while(curr!=NULL)
+        {
+            Node*temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
     bool isPalindrome(Node *head)
     {
         //Your code here
         
         // using with the help of vector (APPROACH 1) using o(n) space
         
-        vector<int>v;
-        Node*temp=head;
-        while(temp!=NULL)
-        {
-            v.push_back(temp->data);
-            temp=temp->next;
-        }
+    //     vector<int>v;
+    //     Node*temp=head;
+    //     while(temp!=NULL)
+    //     {
+    //         v.push_back(temp->data);
+    //         temp=temp->next;
+    //     }
         
-        // now the question will become of array (means that array is palindrome or not)
+    //     // now the question will become of array (means that array is palindrome or not)
         
-        // do reverse the array
+    //     // do reverse the array
        
-       int low=0;
-       int high=v.size()-1;
-       while(low<high){
-           if(v[low]!=v[high]){
-               return false;
-           }
-           low++;
-           high--;
-       }
-       return true;
+    //   int low=0;
+    //   int high=v.size()-1;
+    //   while(low<high){
+    //       if(v[low]!=v[high]){
+    //           return false;
+    //       }
+    //       low++;
+    //       high--;
+    //   }
+    //   return true;
        
        // this is my approach
        
+       
+       // Approach 2 ( WITHOUT CONVERTING INTO VECTOR OR WITHOUT USING EXTRA SPACE)
+       
+       // play with links
+       
+       Node* middle=middleofll(head);
+       
+       // ab middle ke badd wali list ko reverse kar
+       
+       Node* newhead=rev(middle);
+       
+       // now we have two different linked lists compare the data
+       
+       Node*temp=newhead;
+       
+       while(temp!=NULL)
+       {
+           if(temp->data!=head->data){
+               return false;
+           }
+          
+           temp=temp->next;
+           head=head->next;
+           
+       }
+       return true;
     }
        
 };
