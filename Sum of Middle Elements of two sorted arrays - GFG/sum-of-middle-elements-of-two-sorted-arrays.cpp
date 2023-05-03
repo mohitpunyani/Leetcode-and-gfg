8714cf2c4a527d7916cbe1da364f73fbb4a5258e
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-03 16:50:04 +0530

    Added solution - LeetHub

--- Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
+++ Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -14,29 +14,80 @@ public:
     {
             // code here 
             
-            int i=0;
-            int j=0;
-            int k=n-1;
-            while(i<=k && j<n)
+            // 1. approach -> using o(nlogn)
+            
+            // int i=0;
+            // int j=0;
+            // int k=n-1;
+            // while(i<=k && j<n)
+            // {
+            //     if(ar1[i]>ar2[j])
+            //     {
+            //         swap(ar1[k],ar2[j]);
+            //         j++;
+            //         k--;
+                    
+            //     }
+            //     else{
+            //         i++;
+            //         continue;
+            //     }
+                
+            // }
+            // sort(ar1,ar1+n);
+            // sort(ar2,ar2+n);
+            
+            // int sum=ar1[n-1]+ar2[0];
+            // return sum;
+            
+            // 2. approach -> using binary search o(logn)
+            
+            // you can use binary search on any array because both of them are same side
+            
+            // VERY IMPORTANT QUESTION 
+            
+            int low=0;
+            int high=n;
+            
+            int total = n+n;
+            // when length are different then apply binary search on shortest array to avoid index out of board
+            
+            while(low<=high)
             {
-                if(ar1[i]>ar2[j])
+                // initialize the cut or partitions
+                
+                
+                int cut1=low+(high-low)/2;
+                
+                // total required - already present
+                
+                int cut2=n-cut1;
+                
+                // initialize l1,l2,r1,r2
+                int l1=cut1==0?INT_MIN:ar1[cut1-1];
+                int l2=cut2==0?INT_MIN:ar2[cut2-1];
+                int r1=cut1==n?INT_MAX:ar1[cut1];
+                int r2=cut2==n?INT_MAX:ar2[cut2];
+                
+                // we get always even length 
+                if(l1<=r2 and l2<=r1)
                 {
-                    swap(ar1[k],ar2[j]);
-                    j++;
-                    k--;
+                    return max(l1,l2)+min(r1,r2);
                     
                 }
-                else{
-                    i++;
-                    continue;
+                else if(l1>r2)
+                {
+                    high=cut1-1;
+                    
+                }
+                else if(l2>r1)
+                {
+                    low=cut1+1;
+                    
                 }
                 
             }
-            sort(ar1,ar1+n);
-            sort(ar2,ar2+n);
-            
-            int sum=ar1[n-1]+ar2[0];
-            return sum;
+            return 0;
             
     }
 };
