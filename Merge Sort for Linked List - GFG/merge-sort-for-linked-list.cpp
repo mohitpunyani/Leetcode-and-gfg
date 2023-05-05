9b2c036ae6aeb3b2bd5fa0a6a85e8ce7b4cd058c
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-05 20:51:54 +0530

    Added solution - LeetHub

--- Merge Sort for Linked List - GFG/merge-sort-for-linked-list.cpp
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.cpp
@@ -25,90 +25,6 @@ struct Node
 };
 */
 
-
-// class Solution{
-//   public:
-  
-//   Node* middle(Node *head)
-//     {
-//         // using slow and fast pointer theorm
-//         Node*slow=head;
-//         Node*fast=head->next;
-//         while(fast->next!=NULL && fast!=NULL)
-//         {
-//             fast=fast->next->next;
-//             // if(fast->next!=NULL){
-//             //     fast=fast->next;
-//             // }
-//             slow=slow->next;
-//         }
-//         return slow;
-        
-//     }
-//   Node* merge(Node* head1, Node* head2)  
-// {  
-//     // code here
-//     Node*dummynode=new Node(-1);
-//     Node*temp=dummynode;
-//     Node*curr1=head1;
-//     Node*curr2=head2;
-    
-//     while(curr1!=NULL && curr2!=NULL)
-//     {
-//         if(curr1->data<=curr2->data)
-//         {
-//             temp->next=curr1;
-//             temp=curr1;
-//             curr1=curr1->next;
-            
-//         }
-//         else{
-//             temp->next=curr2;
-//             temp=curr2;
-//             curr2=curr2->next;
-//         }
-       
-//     }
-//     while(curr1!=NULL){
-//         temp->next=curr1;
-//         temp=temp->next;
-//         curr1=curr1->next;
-//     }
-//     while(curr2!=NULL){
-//         temp->next=curr2;
-//         temp=temp->next;
-//         curr2=curr2->next;
-//     }
-//     return dummynode->next;
-// }
-//     //Function to sort the given linked list using Merge Sort.
-//     Node* mergeSort(Node* head)
-//     {
-//         // your code here
-        
-//         // similary like to sort array using merge sort
-        
-//         // here we have find middle in different way
-//           if(head==NULL || head->next==NULL)
-//         {
-//         return head;
-//         }
-//         Node *curr=head;
-//         Node *left=NULL;
-//         Node *right=NULL;
-//         Node* mid=middle(curr);
-//         left=head;
-//         right=mid->next;
-//         mid->next=NULL;
-//         left=mergeSort(left); // left call
-//         right=mergeSort(right); // right call
-//         Node *ans=merge(left,right); // complete call
-//         return ans;
-        
-//     }
-// };
-
-
 class Solution{
   public:
     //Function to sort the given linked list using Merge Sort.
@@ -118,12 +34,12 @@ class Solution{
         Node* fast=head->next;
         while(fast!=NULL && fast->next!=NULL)
         {
-            slow=slow->next;
             fast=fast->next;
-            if(fast->next!=NULL){
+            if(fast->next!=NULL)
+            {
                 fast=fast->next;
             }
-            // fast=fast->next->next;
+            slow=slow->next;
         }
         return slow;
     }
@@ -162,7 +78,9 @@ class Solution{
                 temp=right;
                 right=right->next;
         }
+        
         return curr->next;
+        
     }
     Node* mergeSort(Node* head) {
         // your code here
