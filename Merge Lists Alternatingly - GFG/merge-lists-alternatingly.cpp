e00ebd11ed0efd6dc2c5725dbbdc4074f88ba567
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-05 13:28:56 +0530

    Added solution - LeetHub

--- Merge Lists Alternatingly - GFG/merge-lists-alternatingly.cpp
+++ Merge Lists Alternatingly - GFG/merge-lists-alternatingly.cpp
@@ -84,33 +84,21 @@ void mergeList(struct Node **p, struct Node **q)
      
      // first reverse both the linked list
      
-    //  Node*curr1=*p;
-    //  Node*curr2=*q;
+     Node*curr1=*p;
+     Node*curr2=*q;
      
-    //  // linked list are already given in reverse order
+     // linked list are already given in reverse order
      
-    //  while(curr1!=NULL && curr2!=NULL)
-    //  {
-    //      Node*Next1=curr1->next;
-    //      Node*Next2=curr2->next;
-    //      curr1->next=curr2;
-    //      curr2->next=Next1;
-    //      curr1=curr1->next;
-    //      curr2=curr2->next;
-    //  }
-    // *q=curr2;
-// }
-    Node *curr1=*p,*curr2=*q;
-Node *c1=curr1;
-while(curr1!=NULL&&curr2!=NULL)
-{
-Node *temp1=curr1->next;
-Node *temp2=curr2->next;
-curr1->next=curr2;
-curr2->next=temp1;
-curr1=temp1;
-curr2=temp2;
+     while(curr1!=NULL && curr2!=NULL)
+     {
+         Node*Next1=curr1->next;
+         Node*Next2=curr2->next;
+         curr1->next=curr2;
+         curr2->next=Next1;
+         curr1=Next1;
+         curr2=Next2;
+     }
+    *q=curr2;
 }
-*p=c1;
-*q=curr2;
-}
+
+
