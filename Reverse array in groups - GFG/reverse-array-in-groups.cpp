5d68e85076e49876e89042196b6f1f1320b4b617
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-19 23:44:33 +0530

    Added solution - LeetHub

--- Reverse array in groups - GFG/reverse-array-in-groups.cpp
+++ Reverse array in groups - GFG/reverse-array-in-groups.cpp
@@ -10,58 +10,22 @@ using namespace std;
 
 
 class Solution{
-public:
-
-void reversearray(vector<long long> &arr,int low,int high,int size)
-{
-    // pahle complete array ko reverse kiya aur phir rotate kiya n-k se
-    
-        while(low<high)
-        {
+    public:
+    void reverse(vector<long long>&arr,int low,int high){
+        while(low<high){
             swap(arr[low],arr[high]);
             low++;
             high--;
         }
-        return ;
-    
-}
-    //Function to reverse every sub-array group of size k.
-    void reverseInGroups(vector<long long>& arr, int n, int k){
-        // code here
-        
-        //  algo
-        
-        // this is my way to think and i write the code.
-        if(k>=n){
-            reversearray(arr,0,n-1,n);
-        }
-        else
+    }
+    void reverseInGroups(vector<long long>&arr,int n,int k)
+    {
+        for(int i=0;i<n;i=i+k)
         {
-            int low=0;
-            int high=k-1;
-            while(low<high)
-            {
-                reversearray(arr,low,high,n);
-                low=low+k;
-                high=min(low+k-1,n-1);
-                
-                
-                
-            }
+            int low=i;
+            int high=min(i+k-1,n-1);
+            reverse(arr,low,high);
         }
-        
-        // it's not my way to write the code
-        
-        // for(int i=0;i<n;i=i+k)
-        // {
-        //     int  left=i;
-        //     int high=min(i+k-1,n-1);
-        //     reversearray(arr,left,high,n);
-            
-        // }
-        
-        
-        
     }
 };
 
