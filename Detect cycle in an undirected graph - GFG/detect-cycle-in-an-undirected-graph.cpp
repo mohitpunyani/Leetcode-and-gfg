10b923db10a7a9ba4d8c8c5ea25ee482166a8bd1
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-21 09:23:52 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    
+  
+    // make a map of child and parent.
+    
+    bool solve(int node,vector<int>adj[],unordered_map<int,int>&parent,queue<int>&q,
+    unordered_map<int,bool>&visited)
+    {
+        q.push(node);
+        
+        visited[node]=true;
+        
+        parent[node]=-1;
+        
+        while(!q.empty()){
+            
+            int node=q.front();
+            q.pop();
+            
+            for(auto neighbours:adj[node])
+            {
+                if(!visited[neighbours]){
+                    q.push(neighbours);
+                    visited[neighbours]=true;
+                    parent[neighbours]=node;
+                }
+                else if(visited[neighbours]==true && neighbours==parent[node]){
+                    continue;
+                }
+                else if(visited[neighbours]==true && neighbours!=parent[node])
+                {
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+        
+    }
+    bool isCycle(int V, vector<int> adj[])
+    {
+        //  Code here
+         
+        // using bfs
+        
+        // to handle for disconnected graph, we have to loop through this graph
+        
+        unordered_map<int,bool>visited;
+        unordered_map<int,int>parent;
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(!visited[i])
+            {
+                bool ans=solve(i,adj,parent,q,visited);
+                if(ans==1){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+        
+       
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
