7fbcb79a532b3c3b72c91f929afa98dbcd1cd42e
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-22 18:27:55 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -14,43 +14,90 @@ class Solution
         
         // method 1 using queue
         
-        vector<int>distance(V,INT_MAX);
+        // vector<int>distance(V,INT_MAX);
         
-        distance[S]=0;
+        // distance[S]=0;
         
-        queue<int>q;
+        // queue<int>q;
         
-        q.push(S);
+        // q.push(S);
         
-        while(!q.empty())
-        {
-            int top=q.front();
+        // while(!q.empty())
+        // {
+        //     int top=q.front();
             
-            q.pop();
+        //     q.pop();
             
             
-            for(auto it:adj[top])
-            {
-                int node=it[0];
+        //     for(auto it:adj[top])
+        //     {
+        //         int node=it[0];
                 
-                int weight=it[1];
+        //         int weight=it[1];
                 
-                if(distance[node]>distance[top]+weight)
-                {
-                    distance[node]=distance[top]+weight;
-                    q.push(node);
+        //         if(distance[node]>distance[top]+weight)
+        //         {
+        //             distance[node]=distance[top]+weight;
+        //             q.push(node);
                 
-                }
-                else if(distance[node]<=distance[top]+weight)
-                {
-                    continue;
-                }
-            }
-        }
+        //         }
+        //         else if(distance[node]<=distance[top]+weight)
+        //         {
+        //             continue;
+        //         }
+        //     }
+        // }
         
-        return distance;
+        // return distance;
         
-  
+        // METHOD 2 -> DIJKSTRA ALGORITHM USING PRIORITY QUEUE OR SET
+        
+        // using priority queue (this approach is developed from approach 1 )
+        
+        // MAKE A MINHEAP OF DISTANCE AND NODE
+        
+        // SORT TAKES PLACE ON THE BASIS OF FIRST VALUE OF PAIR (WHICH IS DISTANCE)
+        
+         typedef pair<int, int> pi;
+
+         priority_queue<pi, vector<pi>, greater<pi> > pq;
+         
+         
+         pq.push({0,S});
+         
+
+         vector<int>dis(V,INT_MAX);
+         
+         dis[S]=0;
+         
+         while(!pq.empty())
+         {
+             int top=pq.top().second;
+             
+             int distance=pq.top().first;
+             
+             pq.pop();
+             
+             for(auto it:adj[top])
+             {
+                 int node=it[0];
+                 
+                 int weight=it[1];
+                 
+                 if(dis[node] >  weight + distance)
+                 {
+                     dis[node]=weight+distance;
+                     pq.push({dis[node],node});
+                 }
+                 else{
+                     continue;
+                 }
+                 
+             }
+             
+         }
+        
+    return dis;
         
     }
 };
