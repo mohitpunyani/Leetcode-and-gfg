1e949b1e3f0df0662607b2d3f5b06b9454cb4ec6
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-23 11:45:54 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,6 +3,69 @@
 using namespace std;
 
 // } Driver Code Ends
+
+// THIS IS FOR KRUSKAL ALGORITHM
+class DisjointSet
+{
+    public:
+
+
+    // make array of size n+1 , it handles both 1 based and 0 based indexing
+
+    vector<int>parent;
+    vector<int>rank;
+    
+    // make a constructor for initialization
+
+    DisjointSet(int n)
+    {
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+
+    int findparent(int node)
+    {
+        if(node==parent[node]){
+            return node;
+        }
+        parent[node]=findparent(parent[node]);
+        return parent[node];
+
+    }
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=findparent(u);
+        int ultimate_parent_v=findparent(v);;
+        if( ultimate_parent_u==ultimate_parent_v)
+        {
+            // that means both are belong for same component
+            return ;
+        }
+        else{
+            if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+            {
+                parent[ultimate_parent_v] =ultimate_parent_u;
+                rank[ultimate_parent_u]++;
+            }
+            else if(rank[ultimate_parent_u]>rank[ultimate_parent_v]){
+                // connect smaller rank to the larger rank
+                parent[ultimate_parent_v]=ultimate_parent_u;
+
+            }
+            else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+            {
+                parent[ultimate_parent_u]=ultimate_parent_v;
+
+            }
+        }
+
+
+    }
+};
 class Solution
 {
 	public:
@@ -11,61 +74,118 @@ class Solution
     {
         // code here
         
-        // PRIM'S ALGORITHM (STRIVER)
-        
-        typedef pair<int,int>pi;
-        
-        priority_queue<pi,vector<pi>,greater<pi>> pq;
-        
-        // make a min heap of weight and node
+        // METHOD 1 -> PRIM'S ALGORITHM (STRIVER)
         
         
+        // typedef pair<int,int>pi;
         
-        unordered_map<int,bool>visited;;
+        // priority_queue<pi,vector<pi>,greater<pi>> pq;
         
-        pq.push({0,0});
-        
-        int sum=0;
+        // // make a min heap of weight and node
         
         
-        while(!pq.empty())
-        {
-            int weight=pq.top().first;
+        
+        // unordered_map<int,bool>visited;;
+        
+        // pq.push({0,0});
+        
+        // int sum=0;
+        
+        // while(!pq.empty())
+        // {
+        //     int weight=pq.top().first;
             
-            int node=pq.top().second;
+        //     int node=pq.top().second;
             
-            pq.pop();
+        //     pq.pop();
             
             
-            if(!visited[node])
-            {
-                sum+=weight;
-                visited[node]=true;
+        //     if(!visited[node])
+        //     {
+        //         sum+=weight;
+        //         visited[node]=true;
                 
-                for(auto it:adj[node])
-                {
+        //         for(auto it:adj[node])
+        //         {
                     
-                    int node=it[0];
+        //             int node=it[0];
                     
-                    int weight=it[1];
+        //             int weight=it[1];
                     
-                    if(visited[node]==false)
-                    {
-                        pq.push({weight,node});
-                    }
-                    else{
-                        continue;
-                    }
+        //             if(visited[node]==false)
+        //             {
+        //                 pq.push({weight,node});
+        //             }
+        //             else{
+        //                 continue;
+        //             }
                     
                     
-                }
+        //         }
+                
+        //     }
+            
+            
+        // }
+        
+        // return sum;
+        
+        
+        // METHOD 2 -> KRUKSHAL ALGORITHM 
+        
+        // TWO CONCEPTS
+        
+        // 1. DISJOINT SET 
+        
+        //  two important operations performed by disjoint set
+        
+        //   a. find parent or find set
+        
+        //   b. union or union set
+        
+        // 2. UNION BY RANK AND PATH COMPRESSION 
+        
+        //  above two operation also performed by union by rank and path compression in a efficient manner.
+        
+        
+        //  make a adjaency list 
+        
+        vector<pair<int,pair<int,int>>>edges;
+        
+        // we have to sort on the basis of weight.
+        
+        for(int i=0;i<V;i++)
+        {
+            for(auto x:adj[i])
+            {
+                int adjNode=x[0];
+                int weight=x[1];
+                int node=i;
+                
+                edges.push_back({weight,{node,adjNode}});
                 
             }
             
-            
         }
-        return sum;
         
+        DisjointSet ds(V);
+        sort(edges.begin(),edges.end());
+        
+        int mstWt=0;
+        for(auto it:edges)
+        {
+            
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            
+            if(ds.findparent(u)!=ds.findparent(v)){
+                mstWt+=wt;
+                ds.union_by_rank(u,v);
+            }
+        }
+        
+        return mstWt;
         
     }
 };
