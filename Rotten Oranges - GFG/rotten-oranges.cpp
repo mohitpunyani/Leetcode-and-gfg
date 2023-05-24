1fb74a1c95f2fa6aef373478b4cba7486b35e2d8
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-24 21:29:20 +0530

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    
+    // for understand this dfs you have to dry run 
+    
+    // NOTE -> WE USE TIME VARIABLE OR TIME +1 SO THAT WE DON'T REACH THAT NODE AGAIN AND       AGAIN 
+    void dfs(vector<vector<int>>&grid,int i,int j,int rows,int cols,int time)
+    {
+        // IF WE FIND EMPTY CELL THAN WE HAVE TO RETURN 
+        
+if(i<0 || j<0 || i>=rows || j>=cols || grid[i][j]==0 || (grid[i][j]>1 && grid[i][j]<time))
+        {
+           // THE LAST OR CONDITION WE APPLY SO THAT WE DON'T REACH THAT NODE AGAIN 
+            return ;
+        }
+        
+     // dfs call for all four directions if we find a fresh orange
+        
+        grid[i][j]=time;
+        
+     dfs(grid,i,j-1,rows,cols,time+1);
+    
+    dfs(grid,i,j+1,rows,cols,time+1);
+    
+    dfs(grid,i+1,j,rows,cols,time+1);
+    
+    dfs(grid,i-1,j,rows,cols,time+1);
+    
+    return ;
+}       
+        
+    
+    int orangesRotting(vector<vector<int>>& grid)
+    {
+        int rows=grid.size();
+        
+        int time=2;
+        int cols=grid[0].size();
+        
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    // 2 represent rotten orange
+                    
+                    dfs(grid,i,j,rows,cols,time);
+                }
+                
+            }
+        }
+
+        
+        for(auto i:grid)
+        {
+            for(auto j:i)
+            {
+                if(j==1)
+                {
+                    // it means we find a fresh orange so return -1
+                    return -1;
+                }
+                // else{
+                    time=max(time,j);
+                // }
+                
+            }
+            
+        }
+        return time-2;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
