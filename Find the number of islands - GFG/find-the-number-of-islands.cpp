922598af71a74afbdcf70cd916d9430cb2ca2c22
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-24 11:25:35 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,48 +6,108 @@ using namespace std;
 class Solution {
   public:
   
-  void dfs(vector<vector<char>>&grid,int i , int j)
+//   void dfs(vector<vector<char>>&grid,int i , int j)
+//   {
+//       if(i<0 || j<0 || i>=grid.size()  || j>=grid[0].size() || grid[i][j]=='0')
+//       {
+//           return ;
+//       }
+        
+//         // call is 8 directions
+        
+//         grid[i][j]='0';  // reset to zero because this is already included or visited.
+        
+//         //  mark as visited for visited mark make it to zero
+        
+        
+        
+//         dfs(grid,i-1,j);
+//         dfs(grid,i+1,j);
+//         dfs(grid,i,j-1);
+//         dfs(grid,i,j+1);
+//         dfs(grid,i-1,j+1);
+//         dfs(grid,i+1,j-1);
+//         dfs(grid,i-1,j-1);
+//         dfs(grid,i+1,j+1);
+//         return ;
+//   }
+  
+  
+       int x[8]={0,-1,-1,-1,0,1,1,1};
+       int y[8]={-1,-1,0,1,1,1,0,-1};
+       
+  void dfs_efficient(vector<vector<int>>&visited,vector<vector<char>>&grid,int i,int j,int n,int m)
   {
-      if(i<0 || j<0 || i>=grid.size()  || j>=grid[0].size() || grid[i][j]=='0')
+      // standard method
+      
+     
+       
+      // make a vector for 8 directions
+      
+      visited[i][j]=1;
+      for(int k=0;k<8;k++)
       {
-          return ;
+          int row=i+x[k];
+          int col=j+y[k];
+          
+          if(row>=0 && row<n && col>=0 && col<m && visited[row][col]!=1 &&
+            grid[row][col]=='1'){
+            dfs_efficient(visited,grid,row,col,n,m);
+            }
       }
-        
-        // call is 8 directions
-        
-        grid[i][j]='0';  // reset to zero because this is already included
-        
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
-        dfs(grid,i-1,j+1);
-        dfs(grid,i+1,j-1);
-        dfs(grid,i-1,j-1);
-        dfs(grid,i+1,j+1);
-        return ;
+    //   return ;
   }
 
     // Function to find the number of islands.
+    
+    
     int numIslands(vector<vector<char>>& grid) 
     {
-        // Code here
         
-        // no. of connected components
+    //     // Code here
         
-        // using dfs 
+    //     // no. of connected components
+        
+    //     // USING DFS
+        
+    //     int islands=0;
+    //     for(int i=0;i<grid.size();i++){
+    //         for(int j=0;j<grid[i].size();j++)
+    //         {
+    //             if(grid[i][j]=='1')
+    //             {
+    //                 // if it is land than we have to proceed further
+                    
+    //                 dfs(grid,i,j);
+    //                 islands++;
+                    
+    //             }
+                
+    //         }
+            
+    //     }
+    //     return islands;
+        
+        // EFFICIENT WAY TO WRITE DFS (IT IS DEVELOPED BY ABOVE DFS APPROACH)
+        
+        
+        // making a visited vector having n rows and m columns
         
         int islands=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++)
-            {
-                if(grid[i][j]=='1')
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && visited[i][j]==0)
                 {
-                    dfs(grid,i,j);
                     islands++;
-                    
+                    dfs_efficient(visited,grid,i,j,n,m);
                 }
-                
             }
             
         }
