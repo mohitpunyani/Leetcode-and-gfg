2594135a63b49bcb390154698880408de1d94990
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-24 10:41:20 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,41 +6,28 @@ using namespace std;
 class Solution {
   public:
   
-//   void dfs(vector<vector<char>>&grid,int i , int j)
-//   {
-//       if(i<0 || j<0 || i>=grid.size()  || j>=grid[0].size() || grid[i][j]=='0')
-//       {
-//           return ;
-//       }
+  void dfs(vector<vector<char>>&grid,int i , int j)
+  {
+      if(i<0 || j<0 || i>=grid.size()  || j>=grid[0].size() || grid[i][j]=='0')
+      {
+          return ;
+      }
         
-//         // call is 8 directions
+        // call is 8 directions
         
-//         grid[i][j]=0;  // reset to zero because this is already included
+        grid[i][j]='0';  // reset to zero because this is already included
         
-//         dfs(grid,i-1,j);
-//         dfs(grid,i+1,j);
-//         dfs(grid,i,j-1);
-//         dfs(grid,i,j+1);
-//         dfs(grid,i-1,j+1);
-//         dfs(grid,i+1,j-1);
-//         dfs(grid,i-1,j-1);
-//         dfs(grid,i+1,j+1);
-//   }
-  void dfs(vector<vector<char>>& v,int i,int j)
-    {
-        if(i<0 || j<0 || i>=v.size() || j>=v[0].size() || v[i][j]=='0')
-            return;
-        
-        v[i][j]='0';// reset to 0 as this is already included
-        dfs(v,i-1,j);
-        dfs(v,i+1,j);
-        dfs(v,i,j-1);
-        dfs(v,i,j+1);
-        dfs(v,i-1,j+1);
-        dfs(v,i+1,j-1);
-        dfs(v,i-1,j-1);
-        dfs(v,i+1,j+1);
-    }
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j+1);
+        dfs(grid,i+1,j-1);
+        dfs(grid,i-1,j-1);
+        dfs(grid,i+1,j+1);
+        return ;
+  }
+
     // Function to find the number of islands.
     int numIslands(vector<vector<char>>& grid) 
     {
@@ -67,15 +54,7 @@ class Solution {
         return islands;
         
         
-        //   int res=0;
-        // for(int i=0;i<grid.size();i++)
-        //     for(int j=0;j<grid[i].size();j++)
-        //         if(grid[i][j]=='1')// if 1, call the dfs and make res++ to add this island 
-        //         {
-        //             dfs(grid,i,j);
-        //             res++;
-        //         }
-        // return res;
+        
     }
 };
 
