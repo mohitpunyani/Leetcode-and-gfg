c404ab2d12f3a76cdb2409507c1932a40dce30b1
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-05-27 22:02:01 +0530

    Time: 26 ms (43.54%), Space: 16.1 MB (6.28%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -56,6 +56,9 @@ bool  dfs(int node,vector<int>  adj [],unordered_map<int,bool>& visited,unordere
                     // means cycle is present so it means topological sort is not possible
                     return {};
                 }
+                else{
+                    continue;
+                }
                 
             }
             
