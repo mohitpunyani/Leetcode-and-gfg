50c1dcbdf5059e89eb9548b306a965941fda5649
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-06-02 14:48:41 +0530

    Time: 109 ms (93.87%), Space: 37.3 MB (98.30%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    void bfs(int node,vector<int>adj [],vector<bool>&visited)
-    {
-        queue<int>q;
+     int find(int x, vector<int>& parent) {
+ 		if (parent[x] == x) return parent[x];
+        return parent[x] = find(parent[x], parent);
+ 	}
+//     void bfs(int node,vector<int>adj [],vector<bool>&visited)
+//     {
+//         queue<int>q;
         
-        q.push(node);
+//         q.push(node);
         
-        visited[node]=true;
+//         visited[node]=true;
         
-        while(!q.empty())
-        {
-            int top=q.front();
+//         while(!q.empty())
+//         {
+//             int top=q.front();
             
-            q.pop();
+//             q.pop();
             
-            for(auto neighbours:adj[top]){
-                if(!visited[neighbours])
-                {
-                    visited[neighbours]=true;
-                    q.push(neighbours);
-                }
-            }
+//             for(auto neighbours:adj[top]){
+//                 if(!visited[neighbours])
+//                 {
+//                     visited[neighbours]=true;
+//                     q.push(neighbours);
+//                 }
+//             }
             
-        }
-        return ;
-    }
+//         }
+//         return ;
+//     }
     int makeConnected(int n, vector<vector<int>>& connections)
     {
         
@@ -35,34 +39,74 @@ public:
         
         // MAKE A ADJACENCY LIST OF UNDIRECTED GRAPH
         
-        vector<int>adj[n];
+//         vector<int>adj[n];
         
-        if(connections.size()<n-1){
-            return -1;
-        }
-        for(auto c:connections)
-        {
-            int u=c[0];
-            int v=c[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+//         if(connections.size()<n-1)
+//         {
+//             // if edges is less than nodes-1 than return -1 
             
-        }
-        int count=0;
+//             // you can't do connections
+            
+//             return -1;
+//         }
+//         for(auto c:connections)
+//         {
+//             int u=c[0];
+//             int v=c[1];
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+            
+//         }
+//         int count=0;
         
-        vector<bool>visited(n,false);
+//         vector<bool>visited(n,false);
         
-        for(int i=0;i<n;i++){
-            if(!visited[i])
-            {
-                bfs(i,adj,visited);
-                count++;
+//         for(int i=0;i<n;i++){
+//             if(!visited[i])
+//             {
+//                 bfs(i,adj,visited);
+//                 count++;
                 
+//             }
+//         }
+//         if(count==0){
+//             return -1;
+//         }
+//         return count-1;
+        
+        
+        // 2ND APPROACH DISJOINT SET APPROACH 
+        
+
+    
+
+        //Initializing parent array with their own parent values
+        vector<int> parent(n);
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+        }
+        
+        //For counting components
+ 		int compo = 0;
+        
+        for (auto& it: connections) {
+ 			int x = find(it[0], parent);
+ 			int y = find(it[1], parent);
+ 			
+            //If parent are same that means they were connected and now again they are being connected so continue/skip
+            if (x == y){
+                continue;
             }
-        }
-        if(count==0){
-            return -1;
-        }
-        return count-1;
+            
+            //If Parent not same, then Union them and increment compo.
+            else {
+                parent[y] = x; //union
+                compo++;
+            }
+ 		}
+        
+        //We know to connect n computers we need n-1 cables so if connections.size() < n-1 return -1
+		//else required - already connected and i.e. (n-1) - (components)
+        return connections.size() < n-1 ? -1 : n-1-compo;
     }
 };
