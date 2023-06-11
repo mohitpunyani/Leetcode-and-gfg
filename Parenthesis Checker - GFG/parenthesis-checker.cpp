e349eecce943149752ec78116515a1d5e95dfafb
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-06-11 13:32:25 +0530

    Added solution - LeetHub

--- Parenthesis Checker - GFG/parenthesis-checker.cpp
+++ Parenthesis Checker - GFG/parenthesis-checker.cpp
@@ -15,30 +15,63 @@ class Solution
         
         // MY APPROACH 
         
-        stack<char>st;
-        for(int i=0;i<x.size();i++)
-        {
-            if(st.empty() && (x[i]==')' or x[i]==']' or x[i]=='}')){
+    //     stack<char>st;
+    //     for(int i=0;i<x.size();i++)
+    //     {
+    //         if(st.empty() && (x[i]==')' or x[i]==']' or x[i]=='}')){
+    //             return false;
+    //         }
+    //         if(x[i]=='(' or x[i]=='[' or x[i]=='{'){
+    //             st.push(x[i]);
+    //         }
+    // else if((st.top()=='(' && !st.empty() && x[i]==')') or (st.top()=='[' && !st.empty() && x[i]==']') or (st.top()=='{' && x[i]=='}'
+    // && !st.empty())){
+    //     st.pop();
+    //     }
+    // else{
+    //     return false;
+    // }
+            
+    //     }
+    //     if(st.size()==0){
+    //         return true;
+    //     }
+    //     return false;
+    
+    
+    // ANOTHER APPROACH USING MAP
+    unordered_map<char,int>mp={{'(',1},{')',-1},{'[',2},{']',-2},{'{',3},{'}',-3}};
+    
+    stack<char>st;
+    
+    for(int i=0;i<x.size();i++)
+    {
+        
+      
+         if(st.empty() && (x[i]==')' or x[i]==']' or x[i]=='}')){
                 return false;
             }
-            if(x[i]=='(' or x[i]=='[' or x[i]=='{'){
+        if(mp[x[i]]>0)
+        {
+                // we find a opening bracket
+                
                 st.push(x[i]);
+        }
+        else{
+            if(mp[x[i]]+mp[st.top()]!=0){
+                return false;
             }
-    else if((st.top()=='(' && !st.empty() && x[i]==')') or (st.top()=='[' && !st.empty() && x[i]==']') or (st.top()=='{' && x[i]=='}'
-    && !st.empty())){
-        st.pop();
+            else{
+                st.pop();
+            }
         }
-    else{
-        return false;
+        
     }
-            
-        }
-        if(st.size()==0){
-            return true;
-        }
-        return false;
+    if(st.size()==0){
+        return true;
     }
-
+    return false;
+}
 };
 
 //{ Driver Code Starts.
