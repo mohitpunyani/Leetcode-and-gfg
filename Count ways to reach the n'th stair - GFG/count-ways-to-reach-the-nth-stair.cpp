79ffbd087e1641aaf9fa40f325a775d8223c95ff
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-06-17 21:10:25 +0530

    Added solution - LeetHub

---
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair.
+    
+    int mod=1000000007;
+    int topdowndp(vector<int>&dp,int n)
+    {
+         if(n==0 or n==1)
+        {
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=(topdowndp(dp,n-1)+topdowndp(dp,n-2))%mod;
+        return dp[n];
+    }
+    
+    // int bottomupdp(int n){
+        
+    //     dp[0]=1;
+    //     dp[1]=1;
+        
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+    int recursive(int n){
+        if(n==0 or n==1)
+        {
+            return 1;
+        }
+        int ans=recursive(n-1)+recursive(n-2);
+        return ans;
+    }
+    int countWays(int n)
+    {
+        // your code here
+        // return recursive(n);
+        
+        
+        // top down dp
+        
+        vector<int>dp(n+1,-1);
+        
+        return topdowndp(dp,n);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}
+
+// } Driver Code Ends
