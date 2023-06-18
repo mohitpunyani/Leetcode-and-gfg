bf28e8b9402074a4849e6089da003a3d1843ffcd
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-06-18 11:27:36 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (19.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,7 +16,6 @@ public:
         
     }
     
-    
     int topdowndp(vector<int>&dp,vector<int>&nums,int index)
     {
         if(index>=nums.size())
@@ -32,13 +31,41 @@ public:
         dp[index]=ans;
         return dp[index];
     }
+    
+    int bottomupdp(vector<int>&nums)
+    {
+        int n=nums.size();
+        
+        if(n==1){
+            return nums[0];
+        }
+        vector<int>dp(n+1,0);
+        
+        dp[0]=nums[0];
+        
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            
+            int include=dp[i-2]+nums[i];
+            
+            int exclude=dp[i-1]+0;
+            
+            dp[i]=max(include,exclude);
+        }
+        return max(dp[n-1],dp[n-2]);
+    }
     int rob(vector<int>& nums)
     {
         // return recursive(nums,0);
         
-        vector<int>dp(nums.size()+1,-1);
-        return topdowndp(dp,nums,0);
+        // vector<int>dp(nums.size()+1,-1);
+        // return topdowndp(dp,nums,0);
         
+        
+        // bottomupdp
+        
+        return bottomupdp(nums);
         
     }
 };
