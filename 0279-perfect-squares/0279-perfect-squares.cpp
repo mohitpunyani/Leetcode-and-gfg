11b96d3aa8d3aa44a450757e93280375daf26e5d
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-06-18 14:11:53 +0530

    Time: 442 ms (18.45%), Space: 9.1 MB (67.99%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -38,13 +38,52 @@ public:
         dp[n]=temp;
         return temp;
     }
+    
+    int bottomupdp(int n)
+    {
+        vector<int>dp(n+1,INT_MAX);
+        
+        if(n==0){
+            return 0;
+        }
+        if(n==1 or n==2){
+            return n;
+        }
+        dp[0]=0; // for 1 
+        dp[1]=1;  // for 1
+        dp[2]=2;   // for 2
+        
+        // store answer on the basis of one-based indexing 
+        
+        for(int i=3;i<=n;i++)
+        {
+            // for 3 to upto n;
+
+            for(int j=1;j<=sqrt(n);j++)
+            {
+                int square=j*j;
+                if(i-square>=0 && dp[i-square]!=INT_MAX)
+                {
+                    int ans=dp[i-square];
+                    dp[i]=min(dp[i],1+ans);
+
+                } 
+                
+            }
+        }
+        return dp[n];
+    }
     int numSquares(int n) 
     {
         // return recursive(n);
         
-        vector<int>dp(n+1,-1);
+        // vector<int>dp(n+1,-1);
         
-        return topdowndp(dp,n);
+        // return topdowndp(dp,n);
+        
+        // bottomupdp
+        
+        return bottomupdp(n);
         
     }
 };
