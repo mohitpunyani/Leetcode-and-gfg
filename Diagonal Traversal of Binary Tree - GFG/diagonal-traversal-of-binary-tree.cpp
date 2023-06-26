b94392f3056c423ed18a0f7ab7bf517c3871263d
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-06-26 17:56:50 +0530

    Added solution - LeetHub

--- Diagonal Traversal of Binary Tree - GFG/diagonal-traversal-of-binary-tree.cpp
+++ Diagonal Traversal of Binary Tree - GFG/diagonal-traversal-of-binary-tree.cpp
@@ -122,31 +122,37 @@ struct Node
 }; */
 
 
+
 vector<int> diagonal(Node *root)
 {
    // your code here
-   vector<int>ans;
    
-   // my thinking my approach 
+   // use level order traversal  like
    
-   // using bfs or queue
+   vector<int> ans;
    
-   queue<Node*>q;
+   queue<Node*> q;
+   
+  if(root==NULL){
+      return {};
+  }
    q.push(root);
+   
    while(!q.empty()){
        
-       int size=q.size();
-    //   while(size--){
-           Node*temp=q.front();
-           q.pop();
-           while(temp!=NULL){
-               ans.push_back(temp->data);
-               if(temp->left!=NULL){
-                   q.push(temp->left);
-               }
-               temp=temp->right;
+       Node*temp=q.front();
+       
+       q.pop();
+       
+       while(temp!=NULL){
+           
+           ans.push_back(temp->data);
+           if(temp->left!=NULL){
+               q.push(temp->left);
            }
-    //   }
+           temp=temp->right;
+       }
+       
    }
    return ans;
 }
