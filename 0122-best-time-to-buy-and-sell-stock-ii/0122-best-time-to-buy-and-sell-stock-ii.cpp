07168c471cec6431037c8a1f3229e3714b0cb03e
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-07-19 16:32:18 +0530

    Time: 8 ms (62.18%), Space: 15.2 MB (30.44%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -46,9 +46,11 @@ class Solution {
     {
         int n=prices.size();
         vector<vector<int>>dp(n+1,vector<int>(2,0));
+        dp[n][0]=0;
+        dp[n][1]=0;
         for(int index=n-1;index>=0;index--)
         {
-            for(int buy=0;buy<2;buy++)
+            for(int buy=0;buy<=1;buy++)
             {
                 int profit=0;
                 if(buy==1)
@@ -62,7 +64,8 @@ class Solution {
                 int sellkaro=prices[index]+dp[index+1][1];
                 int ignorekaro=dp[index+1][0];
                 profit=max(sellkaro,ignorekaro);
-            }
+                // dp[index][buy]=profit;
+                 }
             dp[index][buy]=profit;
             }
         }
