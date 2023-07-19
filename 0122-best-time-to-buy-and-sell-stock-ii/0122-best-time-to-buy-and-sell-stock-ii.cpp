beb7086026026fb97e25cd305ac60f7db0fd54fa
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2023-07-19 16:11:53 +0530

    Time: 19 ms (12.50%), Space: 17.3 MB (11.21%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,91 @@
+class Solution {
+    private:
+    int recursive(int index,int buy,vector<int> &prices,int n){
+        if(index>=n)
+        {
+            return 0;
+        }
+        int profit=0;
+        if(buy==1){
+            // two choice hai buy karo yah ignore karo
+            int buykaro=-prices[index]+recursive(index+1,0,prices,n);
+            int ignorekaro=recursive(index+1,1,prices,n);
+            profit=max(buykaro,ignorekaro);
+        }
+        else{
+            int sellkaro=prices[index]+recursive(index+1,1,prices,n);
+            int ignorekaro=recursive(index+1,0,prices,n);
+            profit=max(sellkaro,ignorekaro);
+        }
+        return profit;
+    }
+    int topdowndp(int index,int buy,vector<int> &prices,int n,vector<vector<int>> &dp)
+    {
+        if(index>=n){
+            return 0;
+        }
+        int profit=0;
+        if(dp[index][buy]!=-1){
+            return dp[index][buy];
+        }
+        if(buy==1){
+            // two choice hai buy karo yah ignore karo
+            int buykaro=-prices[index]+topdowndp(index+1,0,prices,n,dp);
+            int ignorekaro=topdowndp(index+1,1,prices,n,dp);
+            profit=max(buykaro,ignorekaro);
+        }
+        else{
+            int sellkaro=prices[index]+topdowndp(index+1,1,prices,n,dp);
+            int ignorekaro=topdowndp(index+1,0,prices,n,dp);
+            profit=max(sellkaro,ignorekaro);
+        }
+        dp[index][buy]=profit;
+        return dp[index][buy];
+    }
+    int bottomupdp(vector<int> &prices)
+    {
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int buy=0;buy<2;buy++)
+            {
+                int profit=0;
+            if(buy==1)
+                {
+            // two choice hai buy karo yah ignore karo
+                int buykaro=-prices[index]+dp[index+1][0];
+                int ignorekaro=dp[index+1][1];
+                profit=max(buykaro,ignorekaro);
+                }
+            if(buy==0){
+                int sellkaro=prices[index]+dp[index+1][1];
+                int ignorekaro=dp[index+1][0];
+                profit=max(sellkaro,ignorekaro);
+            }
+            dp[index][buy]=profit;
+            }
+        }
+        return dp[0][1];
+    }
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int n=prices.size();
+        
+        // 1 indicates that sabse pahle hum stock buy karenga
+        // return recursive(0,1,prices,n);
+
+        // topdowndp 2d dp
+
+        //we have 2 columns
+
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        return topdowndp(0,1,prices,n,dp);
+
+        // bottomupdp
+
+        // return bottomupdp(prices);
+    }
+    
+};
