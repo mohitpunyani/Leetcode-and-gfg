01439004d964f057f8b6d28ca6cee3221a4e2c0d
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2024-03-02 16:41:15 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (60.63%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,24 +2,26 @@
 
 class Solution {
 public:
-   
-    void dfs(vector<int> &nums, int index, vector<int>&curr, vector<vector<int>>&res)
+    
+    vector<vector<int>>res;
+    void dfs(vector<int> &nums, int index, vector<int>&curr)
     {
-        if(index>nums.size()){
+        if(index>nums.size())
+        {
             return ;
         }
         res.push_back(curr);
         for(int i=index; i<nums.size();i++)
         {
             curr.push_back(nums[i]);
-            dfs(nums,i+1,curr,res);
+            dfs(nums,i+1,curr);
             curr.pop_back();
         }
     }
      vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
+        // vector<vector<int>> res;
         vector<int> curr;
-        dfs(nums,0,curr,res);
+        dfs(nums,0,curr);
         return res;
        
     }
