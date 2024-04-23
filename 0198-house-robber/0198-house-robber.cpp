a5a9c6c9c4f24e677c5e6f6c36f5f245d9da7e48
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2024-04-23 21:49:50 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (33.80%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,101 +1,61 @@
 class Solution {
 public:
     
-    // based on inclusion exclusion principle
-    
-    int recursive(vector<int>&nums,int index)
+    int solve(vector<int>&nums,int index)
     {
         if(index>=nums.size())
         {
             return 0;
         }
-        int include=nums[index]+recursive(nums,index+2);
-        int exclude=recursive(nums,index+1)+0;
-        int ans=max(include,exclude);
-        return ans;
         
+        int include=solve(nums,index+2)+nums[index];
+        
+        int exclude=solve(nums,index+1)+0;
+        
+        int ans=max(include,exclude);
+        
+        return ans;
     }
     
-    int topdowndp(vector<int>&dp,vector<int>&nums,int index)
+    int topdowndp(vector<int>&nums,int index,vector<int>&dp)
     {
+        
         if(index>=nums.size())
         {
             return 0;
         }
-        if(dp[index]!=-1){
+         
+        if(dp[index]!=-1)
+        {
             return dp[index];
         }
-        int include=nums[index]+topdowndp(dp,nums,index+2);
-        int exclude=topdowndp(dp,nums,index+1)+0;
+        
+        int include=topdowndp(nums,index+2,dp)+nums[index];
+        
+        int exclude=topdowndp(nums,index+1,dp)+0;
+        
         int ans=max(include,exclude);
+        
         dp[index]=ans;
+        
         return dp[index];
     }
     
-    int bottomupdp(vector<int>&nums)
+    int rob(vector<int>& nums) 
     {
+        // RECURSIVE
+        
+        // return solve(nums,0);
+        
+        // TOPDOWN
+        
         int n=nums.size();
         
-        if(n==1){
-            return nums[0];
-        }
-        vector<int>dp(n,0);
-        
-        dp[0]=nums[0];
-        
-        dp[1]=max(nums[0],nums[1]);
-        
-        for(int i=2;i<n;i++)
-        {
-            // for size 3 
+        vector<int>dp(n+1,-1);
             
-            int include=dp[i-2]+nums[i];
-            
-            int exclude=dp[i-1]+0;
-            
-            dp[i]=max(include,exclude);
-        }
-        return dp[n-1];
-    }
+        return topdowndp(nums,0,dp);
     
-    int space_optimization(vector<int>&nums)
-    {
-        int n=nums.size();
-         if(n==1){
-            return nums[0];
-        }
-        int prev1=nums[0];
         
-        int prev2=max(nums[0],nums[1]);
-        
-        for(int i=2;i<n;i++){
-            
-            
-            int include=prev1+nums[i];
-            
-            int exclude=prev2+0;
-            int ans=max(include,exclude);
-            
-            prev1=prev2;
-            
-            prev2=ans;
-        }
-        return prev2;
-        
-    }
-    int rob(vector<int>& nums)
-    {
-        // return recursive(nums,0);
-        
-        // vector<int>dp(nums.size()+1,-1);
-        // return topdowndp(dp,nums,0);
-        
-        
-        // bottomupdp
-        
-        // return bottomupdp(nums);
-        
-        return space_optimization(nums);
         
     }
 };
