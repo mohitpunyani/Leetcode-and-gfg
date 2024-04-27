8830f8a80a65e754cff3a8f6e22755505af2faf4
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2024-04-27 15:48:07 +0530

    Time: 131 ms (19.31%), Space: 11.5 MB (21.46%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,89 +1,72 @@
 class Solution {
 public:
-    int recursive(int n){
-        if(n==0){
+    
+    int recur(int n)
+    {
+        if(n==0)
+        {
             return 0;
         }
+        
         if(n==1){
             return 1;
         }
         
-        int temp=INT_MAX;
+        int mx=INT_MAX;
+        
         for(int i=1;i<=sqrt(n);i++)
         {
-            int square=i*i;
-            int ans=recursive(n-square);
-            temp=min(temp,1+ans);
-        }
-        return temp;
-    }   
-    int topdowndp(vector<int>&dp,int n){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
+            
+            int ans=recur(n-i*i);
+            mx=min(mx,1+ans);
+
         }
         
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int temp=INT_MAX;
-        for(int i=1;i<=sqrt(n);i++)
-        {
-            int square=i*i;
-            int ans=topdowndp(dp,n-square);
-            temp=min(temp,1+ans);
-        }
-        dp[n]=temp;
-        return temp;
+        return mx;
     }
     
-    int bottomupdp(int n)
+    int topdowndp(int n,vector<int>&dp)
     {
-        vector<int>dp(n+1,INT_MAX);
         
-        if(n==0){
+        if(n==0)
+        {
             return 0;
         }
-        if(n==1 or n==2){
-            return n;
+        
+        if(n==1){
+            return 1;
         }
-        dp[0]=0; // for 0 
-        dp[1]=1;  // for 1
-        dp[2]=2;   // for 2
         
-        // store answer on the basis of one-based indexing 
-        
-        for(int i=3;i<=n;i++)
+        if(dp[n]!=-1)
         {
-            // for 3 to upto n;
-
-            for(int j=1;j<=sqrt(n);j++)
-            {
-                int square=j*j;
-                if(i-square>=0 && dp[i-square]!=INT_MAX)
-                {
-                    int ans=dp[i-square];
-                    dp[i]=min(dp[i],1+ans);
-
-                } 
-                
-            }
+            return dp[n];
         }
+        int mx=INT_MAX;
+        
+        for(int i=1;i<=sqrt(n);i++)
+        {
+            
+            int ans=topdowndp(n-i*i,dp);
+            mx=min(mx,1+ans);
+
+        }
+        dp[n]=mx;
+        
         return dp[n];
+        
+        
+        
     }
     int numSquares(int n) 
     {
-        // return recursive(n);
         
-        // vector<int>dp(n+1,-1);
+        // recursive
         
-        // return topdowndp(dp,n);
+        // return recur(n);
         
-        // bottomupdp
+        vector<int>dp(n+1,-1);
         
-        return bottomupdp(n);
+        return topdowndp(n,dp);
         
     }
 };
