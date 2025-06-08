af2f8c682394660cf1a484a8201306d411fc40a9
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-08 16:52:54 +0530

    Time: 88 ms (22.17%), Space: 42.6 MB (16.66%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,9 +5,10 @@ public:
     {
        int n=nums.size();
        sort(nums.begin(),nums.end());
+         set<vector<int>> s;
        for(int i=0;i<=n-3;i++)
        {
-        if(i >0 && nums[i] == nums[i - 1]) continue;
+        if(i>0 && nums[i] == nums[i - 1]) continue;
         int start=i+1;
         int end=n-1;
         while(start<end)
@@ -15,15 +16,26 @@ public:
             int sum=nums[i]+nums[start]+nums[end];
             if(sum==0)
             {
-                vector<int>temp;
-                temp.push_back(nums[i]);
-                temp.push_back(nums[start]);
-                temp.push_back(nums[end]);
-                ans.push_back(temp);
+                // vector<int>temp;
+                // temp.push_back(nums[i]);
+                // temp.push_back(nums[start]);
+                // temp.push_back(nums[end]);
+                // ans.push_back(temp);
+
+                // start++;
+                // end--;
+
+                // to handle duplicates
+
+                // while(start < end && nums[start] == nums[start - 1]) start++;
+                // while(start < end && nums[end] == nums[end + 1]) end--;
+
+                // another way to handle duplicate using set
+
+                s.insert({nums[i], nums[start], nums[end]});
                 start++;
                 end--;
-                while(start < end && nums[start] == nums[start - 1]) start++;
-                while(start < end && nums[end] == nums[end + 1]) end--;
+
             }
             else if(sum>0)
             {
@@ -34,6 +46,9 @@ public:
 
             }
         }
+       }
+       for(auto a:s){
+        ans.push_back(a);
        }
        return ans;
 
