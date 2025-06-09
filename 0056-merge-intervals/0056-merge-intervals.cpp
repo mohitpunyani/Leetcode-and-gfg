66837386ee26baf00050ac31524440b6f4e4a0e3
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-09 20:58:01 +0530

    Time: 7 ms (43.12%), Space: 23.9 MB (34.39%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals)
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
     {
-        // sort on the basis of end
-        
+        vector<vector<int>>ans;
+        vector<int>temp;
+        int n=intervals.size();
         sort(intervals.begin(),intervals.end());
-        
-        vector<vector<int>> ans;
-        vector<int> temp;
         int first=intervals[0][0];
         int second=intervals[0][1];
-        for(int i=1;i<intervals.size();i++)
+        for(int i=1;i<n;i++)
         {
-            if(second>=intervals[i][0])
-            {
-                second=max(second,intervals[i][1]);
-                
+            int curr_first=intervals[i][0];
+            int curr_second=intervals[i][1];
+            if(second>=curr_first){
+                second=max(second,curr_second);
             }
             else{
-                
                 temp.push_back(first);
                 temp.push_back(second);
                 ans.push_back(temp);
-                first=intervals[i][0];
-                second=intervals[i][1];
                 temp.clear();
-                
+                first=curr_first;
+                second=curr_second;
+
             }
-            
+
         }
+        temp.clear();
         temp.push_back(first);
         temp.push_back(second);
         ans.push_back(temp);
