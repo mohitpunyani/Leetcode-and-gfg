74007524e3552f481d9a0409fd410ba092f8155f
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-13 22:03:53 +0530

    Time: 4 ms (64.68%), Space: 13.6 MB (9.09%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int romanToInt(string s)
+    int romanToInt(string s) 
     {
+        map<char,int>mp;
+        mp.insert({{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}});
+        int n=s.size();
         int ans=0;
-        unordered_map<char,int>mp;
-        mp.insert({'I',1});
-        mp.insert({'V',5});
-        mp.insert({'X',10});
-        mp.insert({'L',50});
-        mp.insert({'C',100});
-        mp.insert({'D',500});
-        mp.insert({'M',1000});
-        
-    for(int i=0;i<s.size();i++)
-    {
-        
-        if(mp[s[i]]<mp[s[i+1]])
+        for(int i=0;i<n;i++)
         {
-            ans=ans+mp[s[i+1]]-mp[s[i]];
-            i++;
-            
+            int x=mp[s[i]];
+            int y=mp[s[i+1]];
+            if(x>=y)
+            {
+                ans=ans+x;
+            }
+            else{
+                ans=ans+(y-x);
+                i++;
+            }
+
         }
-        else if(mp[s[i]]>=mp[s[i+1]])
-        {
-            ans=ans+mp[s[i]];
-            
-        }
-    }
         return ans;
     }
 };
