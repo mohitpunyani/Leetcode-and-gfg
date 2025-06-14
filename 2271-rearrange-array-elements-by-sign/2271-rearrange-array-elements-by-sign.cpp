c3293e468fc6a8668ec047074e9e984b810d7d13
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-14 10:27:58 +0530

    Time: 9 ms (40.4%), Space: 138.8 MB (33.3%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,29 +1,59 @@
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums)
-    {
-        // brute force
-        vector<int>pos;
-        vector<int>neg;
-        vector<int>ans;
-        int sz=nums.size();
-        for(int i=0;i<sz;i++){
-            if(nums[i]>=0){
-                pos.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<sz;i++){
-            if(nums[i]<0){
-                neg.push_back(nums[i]);
-            }
-        }
-        sz=pos.size();
-        for(int i=0;i<sz;i++)
+class Solution
+{
+    public:
+        vector<int> rearrangeArray(vector<int> &nums)
         {
-            ans.push_back(pos[i]);
-            ans.push_back(neg[i]);
+           	// brute force
 
+           	//     vector < int>pos;
+           	//     vector < int>neg;
+           	//     vector < int>ans;
+           	//     int sz=nums.size();
+           	//     for(int i=0;i < sz;i++){
+           	//         if(nums[i]>=0){
+           	//             pos.push_back(nums[i]);
+           	//         }
+           	//     }
+           	//     for(int i=0;i < sz;i++){
+           	//         if(nums[i] < 0){
+           	//             neg.push_back(nums[i]);
+           	//         }
+           	//     }
+           	//     sz=pos.size();
+           	//     for(int i=0;i < sz;i++)
+           	//     {
+           	//         ans.push_back(pos[i]);
+           	//         ans.push_back(neg[i]);
+
+           	//     }
+           	//     return ans;
+           	// }
+
+           	// use only one array
+
+            int sz = nums.size();
+            int j = 0;
+            int k = sz - 1;
+            vector<int> posneg(sz);
+            vector<int> temp;
+            for (int i = 0; i < sz; i++)
+            {
+                if (nums[i] >= 0)
+                {
+                    posneg[j] = nums[i];
+                    j++;
+                }
+                else
+                {
+                    posneg[k] = nums[i];
+                    k--;
+                }
+            }
+            for (int i = 0; i < sz/2; i++)
+            {
+                temp.push_back(posneg[i]);
+                temp.push_back(posneg[sz - i - 1]);
+            }
+            return temp;
         }
-        return ans;
-    }
-};
+    };
