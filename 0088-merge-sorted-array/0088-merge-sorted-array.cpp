05063967c6e7934229dcb2c1993b41c7b85fae5e
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-14 09:28:56 +0530

    Time: 0 ms (100%), Space: 12.5 MB (9.03%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,16 +3,45 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
     {
         // brute force
-        // if(n==0 or m==0){
-        //     return ;
+
+        // int i=0;
+        // while(i<n){
+        //     nums1[m+i]=nums2[i];
+        //     i++;
         // }
+        // sort(nums1.begin(),nums1.end());
         
+        // optimized
+        vector<int>temp(m+n);
+
         int i=0;
-        while(i<n){
-            nums1[m+i]=nums2[i];
-            i++;
+        int j=0;
+        int k=0;
+        while(i<m && j<n)
+        {
+            if(nums1[i]<=nums2[j])
+            {
+                temp[k]=nums1[i];
+                i++;
+                k++;
+            }
+            else{
+                temp[k]=nums2[j];
+                j++;
+                k++;
+            }
         }
-        sort(nums1.begin(),nums1.end());
         
+        while(j<n){
+            temp[k]=nums2[j];
+            j++;
+            k++;
+        }
+        while(i<m){
+            temp[k]=nums1[i];
+            i++;
+            k++;
+        }
+        nums1=temp;
     }
 };
