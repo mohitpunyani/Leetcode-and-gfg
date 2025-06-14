6eab174db4c5e025b362b0a64636bd25af5e0b41
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-14 09:33:13 +0530

    Time: 0 ms (100%), Space: 12.3 MB (39.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -12,36 +12,61 @@ public:
         // sort(nums1.begin(),nums1.end());
         
         // optimized
-        vector<int>temp(m+n);
 
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<m && j<n)
+        // vector<int>temp(m+n);
+
+        // int i=0;
+        // int j=0;
+        // int k=0;
+        // while(i<m && j<n)
+        // {
+        //     if(nums1[i]<=nums2[j])
+        //     {
+        //         temp[k]=nums1[i];
+        //         i++;
+        //         k++;
+        //     }
+        //     else{
+        //         temp[k]=nums2[j];
+        //         j++;
+        //         k++;
+        //     }
+        // }
+        
+        // while(j<n){
+        //     temp[k]=nums2[j];
+        //     j++;
+        //     k++;
+        // }
+        // while(i<m){
+        //     temp[k]=nums1[i];
+        //     i++;
+        //     k++;
+        // }
+        // nums1=temp;
+
+        // both are sorted array so that we can do more optimized by traversing from last
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+          while(i>=0&&j>=0)
         {
             if(nums1[i]<=nums2[j])
             {
-                temp[k]=nums1[i];
-                i++;
-                k++;
+                nums1[k]=nums2[j];
+                j--;
+                k--;
             }
-            else{
-                temp[k]=nums2[j];
-                j++;
-                k++;
+            else
+            {
+                nums1[k]=nums1[i];
+                i--;
+                k--;
             }
         }
-        
-        while(j<n){
-            temp[k]=nums2[j];
-            j++;
-            k++;
-        }
-        while(i<m){
-            temp[k]=nums1[i];
-            i++;
-            k++;
-        }
-        nums1=temp;
+        while(i>=0)
+            nums1[k--]=nums1[i--];
+        while(j>=0)
+            nums1[k--]=nums2[j--];
     }
 };
