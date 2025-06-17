8155e4e1c9a3ea3e5cc61051c2111cf47509f0c9
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-17 21:31:08 +0530

    Time: 33 ms (36.82%), Space: 44.7 MB (39.81%) - LeetHub

---
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,54 @@
+class Solution
+{
+    public:
+int subarrayCountWithAtMostKchar(vector<int>& arr, int k) {
+        int i = 0, j = 0, count = 0;
+        unordered_map<int, int> mp; // create an unordered map to keep track of the count of each integer
+
+        while (j < arr.size())
+         {
+            mp[arr[j]]++; // increment the count of integer arr[j] in the map
+
+            if (mp.size() <= k) { // if the number of distinct integers in the map is less than or equal to k
+                count += j - i + 1; // then update the count with the number of subarrays that can be formed with this window
+                j++; // move the right pointer forward
+            } 
+            else {
+                while (mp.size() > k) 
+                { // if the number of distinct integers in the map is greater than k
+                    mp[arr[i]]--; // decrement the count of the integer arr[i] in the map
+
+                    if (mp[arr[i]] == 0)
+                     { // if the count becomes 0
+                        mp.erase(arr[i]); // then remove the integer from the map
+                    }
+
+                    i++; // move the left pointer forward
+                }
+
+                if (mp.size() <= k) { // after shrinking the window, if the number of distinct integers in the map is less than or equal to k
+                    count += j - i + 1; // then update the count with the number of subarrays that can be formed with this window
+                }
+
+                j++; // move the right pointer forward
+            }
+        }
+
+        return count; // return the count
+    }
+    
+    int countCompleteSubarrays(vector<int> &nums)
+    {
+        int n=nums.size();
+        set<int>s;
+        for(int i=0;i<n;i++){
+            s.insert({nums[i]});
+        }
+        int k=s.size();
+          int a = subarrayCountWithAtMostKchar(nums, k); // get the count of subarrays with at most k distinct integers
+        int b = subarrayCountWithAtMostKchar(nums, k - 1); // get the count of subarrays with at most k-1 distinct integers
+
+        return a-b ; // the difference between these counts gives the count of subarrays with exactly k distinct integers
+        
+    }
+};
