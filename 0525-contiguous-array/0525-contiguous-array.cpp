255d88a4c7a0366d6a57058fc48ecb64beb624bc
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-17 22:09:30 +0530

    Time: 28 ms (36.11%), Space: 88.1 MB (91.8%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -5,7 +5,8 @@ public:
         int sum = 0,longest_subarray = 0;
         for(int i = 0; i < nums.size(); i++)
         {
-            sum += nums[i] == 0 ? - 1 : 1;    
+             nums[i]=nums[i] == 0 ? - 1 : 1;    
+             sum=sum+nums[i];
             if(mp.find(sum) != mp.end())
             {
                 if(longest_subarray < i - mp[sum])
