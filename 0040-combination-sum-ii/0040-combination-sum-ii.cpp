c952d72dca0c498488f5816831e796fef951098d
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-21 18:16:16 +0530

    Time: 34 ms (6.21%), Space: 20.5 MB (6.85%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -12,8 +12,9 @@ public:
         }
         for(int i=idx;i<candidates.size();i++)
         {
-             if(i > idx && candidates[i] == candidates[i-1]) continue; // skip duplicates to avoid duplicates combinations
-             
+             if(i > idx && candidates[i] == candidates[i-1]) continue; 
+            // skip duplicates to avoid duplicates combinations
+
             temp.push_back(candidates[i]);
             solve(candidates,temp,target-candidates[i],i+1);
             temp.pop_back();
