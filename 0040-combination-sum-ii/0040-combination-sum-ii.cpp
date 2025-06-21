a35b91d190242a8e1855e1790735eee1c3f0b0c6
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-21 17:59:47 +0530

    Time: 28 ms (7.52%), Space: 20.6 MB (6.85%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    
-    void solve(int index,int k,vector<int>&arr,vector<int>&temp,vector<vector<int>>&ans)
-{
-    if(k<0){
-        return ;
-        
-    }
-    if(k==0){
-        ans.push_back(temp);
-        return ;
-    }
-    for(int i=index;i<arr.size();i++)
+    vector<vector<int>>ans;
+    void solve(vector<int>&candidates,vector<int>temp,int target,int idx)
     {
-        if(i>index && arr[i]==arr[i-1]) 
-            continue;
-
-        if(arr[i]>k){
-            break;
-            // iska matlab hai ki aage wali values ke liye bhi solution nahi hai 
+        if(target<0){
+            return ;
         }
-        else{
-        temp.push_back(arr[i]);
-        solve(i+1,k-arr[i],arr,temp,ans);
-        temp.pop_back();
-    }
-    }
-    return ;
-    
-}
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        int k=target;
-        vector<int>temp;
-        sort(candidates.begin(),candidates.end());
-        solve(0,k,candidates,temp,ans);
-        // ans.erase(unique(ans.begin(),ans.end()),ans.end());
-        // ans.resize(unique(ans.begin(),ans.end())-ans.begin());
-        // A.erase(unique(A.begin(), A.end()), A.end()); // remove duplicate elements
+        if(target==0){
+            ans.push_back(temp);
+            return ;
+        }
+        for(int i=idx;i<candidates.size();i++)
+        {
+             if(i > idx && candidates[i] == candidates[i-1]) continue; // skip duplicates to avoid duplicates combinations
+             
+            temp.push_back(candidates[i]);
+            solve(candidates,temp,target-candidates[i],i+1);
+            temp.pop_back();
 
+        }
+        return ;
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    {
+        sort(candidates.begin(),candidates.end());
+        vector<int>temp;
+        solve(candidates,temp,target,0);
         return ans;
+        
     }
 };
