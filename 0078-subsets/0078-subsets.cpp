b49a2da35c3ba8c5a9ff936fe1c8831b25a9ea79
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-21 14:11:31 +0530

    Time: 4 ms (12.05%), Space: 15.6 MB (19.42%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,30 +1,27 @@
-
-
 class Solution {
 public:
-    
-    vector<vector<int>>res;
-    vector<int>curr;
-    void dfs(vector<int> &nums, int index)
+    vector<vector<int>>ans;
+
+
+    void solve(vector<int>&nums,vector<int>temp,int idx)
     {
-        if(index>nums.size())
-        {
+        if(idx==nums.size()){
+            ans.push_back(temp);
             return ;
         }
-        res.push_back(curr);
-        for(int i=index; i<nums.size();i++)
-        {
-            curr.push_back(nums[i]);
-            dfs(nums,i+1);
-            curr.pop_back();
-        }
-    }
-     vector<vector<int>> subsets(vector<int>& nums) {
-        // vector<vector<int>> res;
-        // vector<int> curr;
-        dfs(nums,0);
-        return res;
-       
-    }
+        temp.push_back(nums[idx]);
+        solve(nums,temp,idx+1);
+        temp.pop_back();
+        solve(nums,temp,idx+1);
+        return ;
 
+    }
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
+        // inclusion exclusion principle
+        vector<int>op;
+        solve(nums,op,0);
+        return ans;
+        
+    }
 };
