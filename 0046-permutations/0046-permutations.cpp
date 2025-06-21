32c350052e2601a5c13c40d49aa7e87e0d7be821
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-21 15:03:41 +0530

    Time: 1 ms (43.6%), Space: 10.9 MB (31.94%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,21 @@
 class Solution {
-    private:
-    void solve(int index,vector<int> &nums,vector<vector<int> > &ans){
-        if(index==nums.size())
-        {
-            ans.push_back(nums);
-            return ;
-        }
-        for(int j=index;j<nums.size();j++)
-        {
-            // ek case solve kar diya baaki recursion sambhal lega
-            swap(nums[index],nums[j]);
-            solve(index+1,nums,ans);
-            
-            // backtrack
-            swap(nums[index],nums[j]);
-        }
-        return ;
-    }
 public:
-    vector<vector<int>> permute(vector<int>& nums)
-    {
-        vector<vector<int>> ans;
-        solve(0,nums,ans);
+    vector<vector<int>> ans;
+    void solve(vector<int>& nums, int index) {
+        if (index >= nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = index; i < nums.size(); i++) {
+            swap(nums[i], nums[index]);
+            solve(nums, index + 1);
+            swap(nums[i], nums[index]); // backtrack
+        }
+        return;
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        // DFS
+        solve(nums, 0);
         return ans;
     }
 };
