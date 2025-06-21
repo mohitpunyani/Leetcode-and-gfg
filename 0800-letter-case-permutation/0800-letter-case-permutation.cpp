fffc36ff795087dc418e4a54ab12709c4e02af27
Mohit Punyani <95860051+mohitpunyani@users.noreply.github.com>
2025-06-21 16:10:47 +0530

    Time: 3 ms (60.01%), Space: 16.1 MB (7.02%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.cpp
+++ 0800-letter-case-permutation/0800-letter-case-permutation.cpp
@@ -1,44 +1,33 @@
 class Solution {
-    
-    private:
-    
-    void solve(string ip,string op,vector<string> &ans){
-        if(ip.size()==0)
-        {
-            ans.push_back(op);
-            return ;
-        }
-        if(isalpha(ip[0]))
-        {
-            string op1=op;
-            string op2=op;
-            op1.push_back(tolower (ip[0]));
-            op2.push_back(toupper (ip[0]));
-            ip.erase(ip.begin()+0);
-            solve(ip,op1,ans);
-            solve(ip,op2,ans);
-            
-        }
-        // agar alphabet nahi hai toh mere paas ek hi choice hai.
-        
-        else{
-            
-            
-            string op3=op;
-            op3.push_back(ip[0]);
-            ip.erase(ip.begin()+0);
-            solve(ip,op3,ans);
-            
-        }
-    }
 public:
+vector<string>ans;
+void solve(string s,string temp,int idx )
+{
+    if(idx>=s.size()){
+        ans.push_back(temp);
+        return ;
+    }
+    if(isdigit(s[idx]))
+    {
+        // we have only one choice
+        solve(s,temp+s[idx],idx+1);
+
+    }
+    if(isalpha(s[idx]))
+    {
+        char a=tolower(s[idx]);
+        solve(s,temp+a,idx+1);
+        char b=toupper(s[idx]);
+        solve(s,temp+b,idx+1);
+
+    }
+    return ;
+
+}
     vector<string> letterCasePermutation(string s) 
     {
-        vector<string> ans;
-        string op="";
-        solve(s,op,ans);
+        string temp="";
+        solve(s,temp,0);
         return ans;
-        
-        
     }
 };
