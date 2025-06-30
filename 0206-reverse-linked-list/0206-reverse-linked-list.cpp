49322e90e5de906434a168223b31ef02d2c1a1d2
Mohit Punyani <mohitpunyani916@gmail.com>
2025-06-30 11:21:48 +0530

    Time: 0 ms (100%), Space: 13.5 MB (19.06%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,20 +10,44 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        if(head==NULL){
-            return NULL;
-        }
-        ListNode*curr=head;
-        ListNode*prev=NULL;
-        while(curr!=NULL)
-        {
 
-            ListNode*temp=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=temp;            
-        }
-        return prev;
+void recursive(ListNode*&head,ListNode* prev, ListNode*curr)
+   {
+       if(curr==NULL)
+       {
+           head=prev;
+           return ;
+       }
+
+    //    prev=curr;
+    //    curr=curr->next; // RECURSION PLAY WITH POINTERS IN THE FUNCTION CALLING (BECAUSE WE HAVE TO CHANGE POINTERS AT EVERY RECURSIVE CALL)
+       recursive(head,curr,curr->next);
+       curr->next=prev;
+       return ;
+
+   }
+    ListNode* reverseList(ListNode* head)
+    {
+        // if(head==NULL){
+        //     return NULL;
+        // }
+        // ListNode*curr=head;
+        // ListNode*prev=NULL;
+        // while(curr!=NULL)
+        // {
+
+        //     ListNode*temp=curr->next;
+        //     curr->next=prev;
+        //     prev=curr;
+        //     curr=temp;            
+        // }
+        // return prev;
+
+        // RECURSIVE APPROACH
+          ListNode*curr=head;
+        ListNode*prev=NULL;
+        recursive(head,prev,curr);
+        return head;
+
     }
 };
