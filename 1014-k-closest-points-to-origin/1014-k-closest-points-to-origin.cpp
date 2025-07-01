ae62572288286f3dc94135676588215bc18a8b37
Mohit Punyani <mohitpunyani916@gmail.com>
2025-07-01 20:43:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,35 +1,6 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>> pi;
-
-    priority_queue<pi,vector<pi>,greater<pi>> pq;
-    // priority_queue<int,vector<int>,greater<int>> mnheap;
-
-    vector<vector<int>>ans;
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
-    {
-        for(int i=0;i<points.size();i++){
-            int x_val=points[i][0];
-            int y_val=points[i][1];
-
-            x_val=x_val*x_val;
-            y_val=y_val*y_val;
-            
-            int sum=x_val+y_val;
-            int distance_from_origin=sum;
-            pq.push({distance_from_origin,{points[i][0],points[i][1]}});
-            
-        }
-        while(!pq.empty() && k>0)
-        {
-            pair<int,int>p=pq.top().second;
-            vector<int>temp;
-            temp.push_back(p.first);
-            temp.push_back(p.second);
-            ans.push_back(temp);
-            pq.pop();
-            k--;
-        }
-        return ans;
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
     }
 };
