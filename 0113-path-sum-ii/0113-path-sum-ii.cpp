6bc155d5a37a67447dd8d077850a02e13313d217
Mohit Punyani <mohitpunyani916@gmail.com>
2025-07-04 22:30:37 +0530

    Time: 0 ms (100%), Space: 21 MB (48.74%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -4,11 +4,13 @@ public:
         if(!root) return ;
 
         path.push_back(root->val);
-        if(root->left == NULL && root->right == NULL && root->val == k){ 
+        if(root->left == NULL && root->right == NULL && k-root->val == 0){ 
             ans.push_back(path);
         }
-        if(root->left) solution(root->left,ans,path,k - root->val);
-        if(root->right) solution(root->right,ans,path,k - root->val);
+        // if(root->left) 
+        solution(root->left,ans,path,k - root->val);
+        // if(root->right) 
+        solution(root->right,ans,path,k - root->val);
 
         path.pop_back();
     }
