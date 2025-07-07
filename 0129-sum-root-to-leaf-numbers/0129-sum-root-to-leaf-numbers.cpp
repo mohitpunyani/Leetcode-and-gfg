11d479ead6220833e3a1d4db39d29fd07a99c8b9
Mohit Punyani <mohitpunyani916@gmail.com>
2025-07-07 14:28:47 +0530

    Time: 0 ms (100%), Space: 12 MB (45%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -9,43 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-class Solution {
-    public:
+ class Solution {
+public:
     
-    int total_sum=0;
+    int sum=0;
+
+    int ans=0;
+    void solve(TreeNode*root)
+    {
+        if(root==NULL){
+            return ;
+        }
+        if(root->left==NULL && root->right==NULL)
+        {
+            sum=sum*10+root->val;
+            ans=ans+sum;
+            sum=sum/10; // backtrack;
+            return ;
+        }
+        sum=sum*10+root->val;
+        solve(root->left);
+        solve(root->right);
+        sum=sum/10; // backtrack;
+        return ;
+
+    }
     int sumNumbers(TreeNode* root)
     {
-        // right wale par calculation baad me hogi pahle left wale ka soch
+        solve(root);
+        return ans;
         
-        
-        stack<pair<TreeNode*,int>>st;
-        st.push({root,0});
-        
-        while(!st.empty())
-        
-        {
-            
-            pair<TreeNode*,int> p=st.top();
-            
-            st.pop();
-            TreeNode*curr_node=p.first;
-            
-            int curr_sum=p.second;
-            curr_sum=curr_sum*10+curr_node->val;
-                
-           if(curr_node->left==NULL && curr_node->right==NULL)
-           {
-                    total_sum=total_sum+curr_sum;
-           }
-            if(curr_node->left!=NULL){
-                st.push({curr_node->left,curr_sum});
-            }
-            if(curr_node->right!=NULL){
-                st.push({curr_node->right,curr_sum});
-            }
-            
-        }
-        return total_sum;
     }
 };
